e6c4577746e719ffaf0b64e65904bb662d5d5783
arielardani <arielardani20@gmail.com>
2025-11-03 01:16:48 +0700

    Percobaan 4

---
+++ jobsheet8/NilaiKelompok04.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NilaiKelompok04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nilai;
+        float totalNilai, rataNilai;
+        int i = 1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+
+            totalNilai = 0;
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok "+i+" Nilai Rata-Rata " + rataNilai);
+            i++;
+        }
+
+    }
+}
