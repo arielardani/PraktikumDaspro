6e877fd7ac8b5dca9045e5a503c999f850ead628
arielardani <arielardani20@gmail.com>
2025-11-17 18:54:09 +0700

    Percobaan 3

---
+++ jobsheet10/Numbers04.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Numbers04 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+        
+    }
+}
